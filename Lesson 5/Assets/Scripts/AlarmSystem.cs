c42f2d7f7fa3244523368b5b8bbece9701f9c782
Fle2x <dotsfam87@Mail.ru>
2020-09-21 15:25:18 +0500

    Add enum. Rename alarmSound

---
+++ Lesson 5/Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AlarmSystem : MonoBehaviour
+{
+    public AudioSource alarmSound;
+
+    private void Start()
+    {
+        alarmSound = GetComponent<AudioSource>();
+    }
+
+    private IEnumerator ChangeVolume(int startValue, int endValue, TurnOnOffAlarm turnOnOffAlarm)
+    {
+        float time = 0;
+
+        if (turnOnOffAlarm == TurnOnOffAlarm.Play)
+            alarmSound.Play();
+
+        while (time < 1)
+        {
+            float volumeValue = Mathf.Lerp(startValue, endValue, time / 1);
+            time += Time.deltaTime;
+            alarmSound.volume = volumeValue;
+            yield return null;
+        }
+
+        if (turnOnOffAlarm == TurnOnOffAlarm.Stop)
+            alarmSound.Stop();
+    }
+
+    private enum TurnOnOffAlarm
+    {
+        Play = 0,
+        Stop = 1
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(ChangeVolume(0, 1, TurnOnOffAlarm.Play));
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StartCoroutine(ChangeVolume(1, 0, TurnOnOffAlarm.Stop));
+    }
+}
